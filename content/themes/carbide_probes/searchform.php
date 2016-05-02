9b0b964949990ddd95a6163e1f2964e3e91e642e
Ed Heltzel <ginfuru@users.noreply.github.com>
2016-05-02 13:42:26 -0400

    updated search placeholder text to be customized with OP

--- content/themes/carbide_probes/searchform.php
+++ content/themes/carbide_probes/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <ul>
         <li class="append field">
-            <input type="search" class="xwide text input" placeholder="<?php echo esc_attr_x( 'Let\'s find it...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" />
+            <input type="search" class="xwide text input" placeholder="<?php echo ot_get_option('search_text', 'placeholder'); ?>" value="<?php echo get_search_query() ?>" name="s" />
             <button type="submit" class="medium squared primary btn"><i class="fa fa-search"></i></button>
 
         </li>
